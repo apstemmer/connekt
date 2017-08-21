5bb46e3c62b0dbe54d7952669ac4062a3d255018
AlexMontgomerie <am9215@ic.ac.uk>
2017-08-21 21:05:49 +0100

    trying to add menu button

---
+++ front/src/menu.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './menu.css';
+
+export class menu extends Component {
+  render() {
+    return (
+      <div className="menu">
+        <div className="menu-intro">
+          <p>some shit</p>
+        </div>
+      </div>
+    );
+  }
+}
