4518c201de9a1b7c4720dc376152041e26be7247
AlexMontgomerie <am9215@ic.ac.uk>
2017-08-23 22:43:38 +0100

    restored a couple files

---
+++ front/src/components/HomePage.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import '../css/HomePage.css';
+
+/*
+  Description:
+    Homepage-button that is on the top bar. It will take the user back to the
+    home page.
+*/
+
+export class HomePage extends Component {
+  render() {
+    return (
+      <div className="HomePage">
+        <button
+        //onClick={}
+        >
+          HomePage
+        </button>
+      </div>
+    );
+  }
+}
