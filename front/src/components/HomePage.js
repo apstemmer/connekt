72cd48a24383150d6a9696710aa69e025bd891ad
AlexMontgomerie <am9215@ic.ac.uk>
2017-08-31 19:39:17 +0100

    added search bar

--- front/src/components/HomePage.js
+++ front/src/components/HomePage.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import '../css/HomePage.css';
 
 import { NavBar } from './NavBar.js';
+import { SearchBar } from './SearchBar.js';
 /*
   Description:
     Homepage-button that is on the top bar. It will take the user back to the
@@ -10,17 +11,25 @@ import { NavBar } from './NavBar.js';
 
 export class HomePage extends Component {
   render() {
+
+    //
+
     return (
       <div className="HomePage">
         <div className="NavBar">
           <NavBar />
         </div>
         <div className="SearchBar">
+          <SearchBar />
         </div>
         <div className="MainBanner">
-          <img src={require('../images/cute-kittens.jpg')} alt="cat pic" />
+          <img src={require('../images/cute-kittens.jpg')}
+            alt="cat pic"
+            href="/some-link"
+          />
         </div>
         <div className="homePageListings">
+
         </div>
         <div className="FilterAds">
         </div>
