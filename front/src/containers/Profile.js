0bab0f5cbcb348a6d89aae4297e195b47c280384
AlexMontgomerie <am9215@ic.ac.uk>
2017-09-21 13:20:56 +0100

    updating p

--- front/src/containers/Profile.js
+++ front/src/containers/Profile.js
@@ -5,12 +5,52 @@ import { NavBar } from '../components/NavBar.js';
 
 export class Profile extends Component {
   render() {
+	function getProfile(){
+	  return [
+		{
+          name 		  : "AlexMontgomerie",
+          rating  	  : "9000",
+          profilePic  : require('../images/cute-kittens.jpg'),
+          Description : "Muscian and so on"
+        }
+	  ]
+	}
+	
+	var profile=getProfile();
+	  
     return (
       <div className="Profile">
         <div className="NavBar">
           <NavBar />
         </div>
-        <h1>Profile</h1>
+        <div className="Profile-top">
+		  <div className="Profile-top-left">
+			<img src={profile[0].profilePic} />
+		  </div>
+		  <div className="Profile-top-right">
+			<div className="Profile-top-right-header">
+			  <h1>{profile[0].name}</h1>
+			  <h2>{profile[0].rating}</h2>
+			</div>
+			<p>{profile[0].Description}</p>
+		  </div>
+		</div>
+		<div className="Profile-social">
+		  <ul>
+			<li>facebook</li>
+			<li>soundcloud</li>
+			<li>twitter</li>
+		  </ul>
+		</div>
+		<div className="Profile-media">
+		
+		</div>
+		<div className="Profile-ads">
+		
+		</div>
+		<div className="Profile-footer">
+		
+		</div>
       </div>
     );
   }
